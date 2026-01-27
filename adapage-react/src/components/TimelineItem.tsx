da69a66ceec4b37a63765c8ea1fc0051cda12534
Gabriel H. <gabrielhono.o@gmail.com>
2026-01-27 14:47:25 +0100

    timeline stylée

--- adapage-react/src/components/TimelineItem.tsx
+++ adapage-react/src/components/TimelineItem.tsx
@@ -3,12 +3,16 @@ import '../App.css'
 export const TimeLineItem = ({ data }) => (
     <div className="timeline-item">
         <div className="timeline-item-content">
-            <span className="type" style={{ background: data.category.color}}>
-                {data.category.type}
-            </span>
-            <time>{data.year}</time>
-            <p>{data.description}</p>
-            <img src={data.img}/>
+            {data.img && <img src={data.img} alt={data.description} />}
+            <div>
+                <div>
+                    <span className="type" style={{ background: data.category.color}}>
+                        {data.category.type}
+                    </span>
+                    <time>{data.year}</time>
+                </div>
+                <h2>{data.description}</h2>
+            </div>
             <span className="circle"/>
         </div>
     </div>
